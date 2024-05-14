837430f831692f967c2f9ba8c688f8b48fe18e18
luisandresonchain <andres.flores@democracyonchain.com>
2024-05-14 14:56:19 -0500

    Add files via upload

---
+++ provincia.sql
@@ -0,0 +1,29 @@
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(1,'AZUAY');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(2,'BOLIVAR');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(3,'CAÑAR');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(4,'CARCHI');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(5,'COTOPAXI');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(6,'CHIMBORAZO');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(7,'EL ORO');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(8,'ESMERALDAS');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(9,'GUAYAS');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(10,'IMBABURA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(11,'LOJA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(12,'LOS RIOS');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(13,'MANABI');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(14,'MORONA SANTIAGO');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(15,'NAPO');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(16,'PASTAZA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(17,'PICHINCHA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(18,'TUNGURAHUA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(19,'ZAMORA CHINCHIPE');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(20,'GALAPAGOS');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(21,'SUCUMBIOS');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(22,'ORELLANA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(23,'STO DGO TSACHILAS');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(24,'SANTA ELENA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(26,'EUROPA');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(27,'CANADÁ Y ESTADOS UNIDOS');
+insert into public.provincia (id, nombre) OVERRIDING SYSTEM VALUE  values(28,'LATINOAMÉRICA');
+
+
